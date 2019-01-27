a28a9cc2584de62cdba021329bb9ecc1258f2db4
nosyarg <york17a@ncssm.edu>
2019-01-26 22:08:14 -0500

    got message sending working

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pyrebase 
 
-
-def sendmessage():
-        message = {"target": "Renzogbenavides@gmail.com", "message": "HELLO"} 
-        db.child("messages").push(message, user['idToken'])
+def sendmessage(message,target):
+        db.child("messages").push({"target": target, "message": message} , user['idToken'])
+def getmessage():
+        print("FUCK")
 
 config = {  "apiKey": "AIzaSyBcqfP5f1AZ2fDq9ktvNQ9MwXGQeD379a8" ,  "authDomain": "secretmessenger-e9bd3.firebaseapp.com",  "databaseURL": "https://secretmessenger-e9bd3.firebaseio.com",  "storageBucket": "secretmessenger-e9bd3.appspot.com"} 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth() #authenticate a user 
 user = auth.sign_in_with_email_and_password("graysonyork23@gmail.com", "badpassword")
 db = firebase.database()
-sendmessage()
+while(1):
+        print("recipient:")
+        target = input()
+        print("message:")
+        message = input()
+        sendmessage(message,target)
